d794249f3d909046b60019ca616796fa94721ac6
Nicgregg8888 <nicgregg2008@gmail.com>
2025-05-05 16:38:26 +0400

    Delete new padel directory

--- new padel/modules/ball_tracking.py
+++
@@ -1,5 +0,0 @@
-
-from random import randint
-
-def track_ball():
-    return {"Trajectory": "Curved Arc", "Speed (km/h)": randint(60, 120)}
--- new padel/modules/shot_detection.py
+++
@@ -1,5 +0,0 @@
-
-from random import choice
-
-def detect_shot_type():
-    return choice(["Smash", "Volley", "Lob"])
